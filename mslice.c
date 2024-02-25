91266d64a838e169c133714bdbcc7fad8467e55d
Masanori Itoh <masanori.itoh@gmail.com>
2024-02-25 17:56:01 +0900

    Make debug messages written to stderr

--- mslice.c
+++ mslice.c
@@ -57,16 +57,16 @@ struct destinfo *check(struct in_addr *target)
 {
   int i;
   for (i = 0; i < num_destinfo; i++) {
-    if (debug) printf("DEBUG: %d: %08x %08x\n",
+    if (debug) fprintf(stderr, "DEBUG: %d: %08x %08x\n",
                       i,
                       (destinfos[i].dest.s_addr & bitmask(destinfos[i].size)),
                       (target->s_addr  & bitmask(destinfos[i].size)));
     if ((destinfos[i].dest.s_addr & bitmask(destinfos[i].size)) ==
         (target->s_addr & bitmask(destinfos[i].size))) {
-      if (debug) printf("match !\n");
+      if (debug) fprintf(stderr, "DEBUG: match !\n");
       return destinfos + i;
     } else {
-      if (debug) printf("no match \n");
+      if (debug) fprintf(stderr, "DEBUG: no match \n");
     }
   }
   return NULL;
@@ -76,7 +76,7 @@ void show_destinfos()
 {
   int i;
   for (i = 0; i < num_destinfo; i++) {
-    if (debug) printf("DEBUG: %d: %08x %d %08x\n",
+    if (debug) fprintf(stderr, "DEBUG: %d: %08x %d %08x\n",
                       i,
                       destinfos[i].dest.s_addr,
                       destinfos[i].size,
@@ -106,7 +106,7 @@ void parse_env()
   destinfos =  malloc(sizeof(struct destinfo) * MAX_DESTINFO);
   memset(destinfos, 0x0, sizeof(struct destinfo) * MAX_DESTINFO);
 
-  if (debug) printf("DEBUG: MSLICE_TARGETS: %s\n", env);
+  if (debug) fprintf(stderr, "DEBUG: MSLICE_TARGETS: %s\n", env);
 
   /* parse MSLICE_TARGETS and build destinfo array */
   for (i = 0, str1 = env; ; i++, str1 = NULL) {
@@ -114,44 +114,45 @@ void parse_env()
     if (token1 == NULL || i > MAX_DESTINFO) {
       break;
     }
-    if (debug) printf("DEGBUG: %d: %s\n", i, token1);
+    if (debug) fprintf(stderr, "DEBUG: %d: %s\n", i, token1);
 
     for (j = 0, str2 = token1; ; j++, str2 = NULL) {
       token2 = strtok_r(str2, ",", &saveptr2);
-      if (debug) printf("DEBUG(token2): %d: %s\n", j, token2);
+      if (debug) fprintf(stderr, "DEBUG(token2): %d: %s\n", j, token2);
       if (token2 == NULL || j > 1) {
         break;
       }
       if (j == 0) {
         if ((str3 = strchr(token2, (int)'/')) != NULL) {
           netsize = atoi(str3 + 1);
-          if (debug) printf("DEBUG(size): %s (%d)\n", str3 + 1, netsize);
+          if (debug) fprintf(stderr, "DEBUG(size): %s (%d)\n", str3 + 1, netsize);
           dest_addr = strndup(token2, str3 - token2);
         } else {
           dest_addr = token2;
-          if (debug) printf("DEBUG(dest): %d %s\n", j, dest_addr);
+          if (debug) fprintf(stderr, "DEBUG(dest): %d %s\n", j, dest_addr);
         }
       }
       if (j == 1) {
         local_addr = token2;
-        if (debug) printf("DDEBUG(local):  %d: %s\n", j, local_addr);
+        if (debug) fprintf(stderr, "DEBUG(local):  %d: %s\n", j, local_addr);
       }
     }
-    if (debug) printf("DEGBUG2: %s/%d -> %s\n", dest_addr, netsize, local_addr);
+    if (debug) fprintf(stderr, "DEBUG2: %s/%d -> %s\n",
+                       dest_addr, netsize, local_addr);
     ret = inet_aton(dest_addr, &(destinfos[i].dest));
     if (ret <= 0) {
-      if (debug) printf("DEBUG3: %s is not valid\n", dest_addr);
+      if (debug) fprintf(stderr, "DEBUG3: %s is not valid\n", dest_addr);
       continue;
     }
     ret = inet_aton(local_addr, &(destinfos[i].local));
     if (ret <= 0) {
-      if (debug) printf("DEBUG3: %s is not valid\n", local_addr);
+      if (debug) fprintf(stderr, "DEBUG3: %s is not valid\n", local_addr);
       continue;
     }
     destinfos[i].size = netsize;
   }
   num_destinfo = i;
-  if (debug) printf("DEBUG: num_destinfo = %d\n", num_destinfo);
+  if (debug) fprintf(stderr, "DEBUG: num_destinfo = %d\n", num_destinfo);
 }
 
 
@@ -167,13 +168,13 @@ static void init_hook()
   parse_env();
 
   org = (int(*)(int sockfd, const struct sockaddr *addr, socklen_t addrlen))dlsym(RTLD_NEXT, "connect");
-  if (debug) printf("DEBUG: %s: constructor called! %p\n", __func__, org);
+  if (debug) fprintf(stderr, "DEBUG: %s: constructor called! %p\n", __func__, org);
 }
 
 __attribute__((destructor))
 static void fini_hook()
 {
-  if (debug) printf("DEBUG: %s: destructor called!\n", __func__);
+  if (debug) fprintf(stderr, "DEBUG: %s: destructor called!\n", __func__);
 }
 
 
@@ -184,29 +185,29 @@ int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
   socklen_t slen;
   struct destinfo *dinfo = NULL;
 
-  if(debug) printf("DEBUG: %s: hook!\n", __func__);
+  if(debug) fprintf(stderr, "DEBUG: %s: hook!\n", __func__);
 
   if (addr->sa_family != AF_INET) {
     return org(sockfd, addr, addrlen);
   }
 
   ret = getsockname(sockfd, &saddr, &slen);
-  if (debug) printf("DEBUG: ret = %d %08x\n", ret, saddr.sin_addr.s_addr);
+  if (debug) fprintf(stderr, "DEBUG: ret = %d %08x\n", ret, saddr.sin_addr.s_addr);
   dinfo = check((struct in_addr*)&((struct sockaddr_in *)addr)->sin_addr.s_addr);
   /* TODO(thatsdone): call bind(2) if dinfo is not NULL */
   if (dinfo != NULL) {
     if (debug) {
-      printf("DEBUG: %08x is in %08x/%d\n",
-             ((struct sockaddr_in *)addr)->sin_addr.s_addr,
-             dinfo->dest.s_addr,
-             dinfo->size);
+      fprintf(stderr, "DEBUG: %08x is in %08x/%d\n",
+              ((struct sockaddr_in *)addr)->sin_addr.s_addr,
+              dinfo->dest.s_addr,
+              dinfo->size);
     }
     local_saddr.sin_family = addr->sa_family;
     local_saddr.sin_addr.s_addr = dinfo->local.s_addr;
     ret = bind(sockfd, &local_saddr, sizeof(struct sockaddr_in));
-    if (debug) printf("DEBUG: bind(2) returns %d (%d)\n", ret, errno);
+    if (debug) fprintf(stderr, "DEBUG: bind(2) returns %d (%d)\n", ret, errno);
     if (ret < 0) {
-      printf("WARNING: bind(2) failed\n");
+      fprintf(stderr, "WARNING: bind(2) failed\n");
     }
   }
   return org(sockfd, addr, addrlen);
